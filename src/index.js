757476b3f7104bf302ec42f4d310e60d8ddf936d
nspp99b <chromepanthers@gmail.com>
2018-01-28 17:21:30 -0500

    defined components, exported and imported

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// Code goes here
+import { message } from './components/foo';
+import Tweet from './components/tweet';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
